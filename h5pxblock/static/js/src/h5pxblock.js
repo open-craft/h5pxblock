9681e3abf6cdc37baecf83c6c6686bb932bff732
Farhaan Bukhsh <farhaan@opencraft.com>
2023-10-06 23:56:08 +0530

    fix: added the request method
    Signed-off-by: Farhaan Bukhsh <farhaan@opencraft.com>

--- h5pxblock/static/js/src/h5pxblock.js
+++ h5pxblock/static/js/src/h5pxblock.js
@@ -24,46 +24,49 @@ function H5PPlayerXBlock(runtime, element, args) {
                 contentUserData: [{
                     state: args.userData
                 }],
-                ajax: { 
+                ajax: {
                     contentUserDataUrl: contentUserDataUrl
-                }
+                },
+                assetsRequestFetchOptions: {
+                    method: 'GET'
+                },
             }
 
-            return new H5PStandalone.H5P(el, options).then(function(){ 
+            return new H5PStandalone.H5P(el, options).then(function(){
                 $(el).siblings('.spinner-container').find('.spinner-border').hide();
-                $(el).show();        
+                $(el).show();
                 H5P.externalDispatcher.on("xAPI", (event) => {
 
                     let hasStatement = event && event.data && event.data.statement;
                     if (!hasStatement) {
                         return;
                     }
-        
+
                     let statement = event.data.statement;
                     let validVerb = statement.verb && statement.verb.display && statement.verb.display['en-US'];
                     if (!validVerb) {
                         return;
                     }
-        
+
                     let isCompleted = statement.verb.display['en-US'] === 'answered' ||
                         statement.verb.display['en-US'] === 'completed';
                         let isChild = statement.context && statement.context.contextActivities &&
                         statement.context.contextActivities.parent &&
                         statement.context.contextActivities.parent[0] &&
                         statement.context.contextActivities.parent[0].id;
-        
+
                     // Store only completed root events.
                     if (isCompleted && !isChild) {
                         $.ajax({
                             type: "POST",
                             url: contentxResultSaveUrl,
                             data: JSON.stringify(event.data.statement)
-                        });    
+                        });
                     }
                     // uncomment to see all xAPI events triggered by H5P content
                     //console.log("xAPI event: ", event);
                 });
-    
+
             });
         }
     };
